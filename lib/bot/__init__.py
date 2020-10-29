7e65b9c74f8068da65fe9f894f9189aa20a5a15d
w60083 <49841198+w60083@users.noreply.github.com>
2020-10-29 16:35:55 +0100

    Commit n2
    Added DB COGS Handling ERROR Scheduling tasks , embeds

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,19 +1,36 @@
+from asyncio import sleep
+from glob import glob
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
 from discord import Embed , File
 from datetime import datetime
+from discord.ext.commands import CommandNotFound
+from apscheduler.triggers.cron import CronTrigger
 
+from ..db import db
 PREFIX = "+"
 OWNER_IDS = [381031540899053568]
+COGS = [path.split("\\")[-1][-3] for path in glob("./lib/cogs/*.py")]
 
+class Ready(object):
+	def __init__(self):
+		for cog in COGS:
+			setattr(self,cog,False)
+
+	def ready_up(self,cog):
+		setattr(self,cog,True)
+		print(f"{cog} cog ready")
+
+	def all_ready(self):
+		return all([getattr(self,cog) for cog in COGS])
 
 class Bot(BotBase):
 	def __init__(self):
 		self.PREFIX = PREFIX
 		self.ready = False
-		
+		self.cogs_ready=Ready()
 		self.scheduler=AsyncIOScheduler()
-
+		db.autosave(self.scheduler)
 		super().__init__(command_prefix=PREFIX,owner_ids=OWNER_IDS)
 
 	def run(self,version):
@@ -32,10 +49,31 @@ class Bot(BotBase):
 	async def on_disconnect(self):
 			print("bot dissconnected")
 
+	async def on_error(self,err,*args,**kwargs):
+    		if err == "on_command_error":
+    			await args[0].send("something went wrong")
+
+    		else:	
+    			channel = self.get_channel(771316587935563808)
+    			await channel.send("ERROR")
+    		raise
+
+	async def on_command_error(self,ctx,exc):
+
+		if isinstance(exv,CommandNotFound):
+			pass
+
+		else:
+			raise exc.original
+			
+
+	
+
 	async def on_ready(self):
 			if not self.ready:
 				self.ready = True
 				print("bot ready")
+				self.stdout = self.get_channel(771316587935563808)
 				channel = self.get_channel(771316587935563808)
 				await channel.send("Hi Guys, Ganz is my KING!!!")
 				embed = Embed(title="Im here", description="Bot is ready",colour=0xFF0000, timestamp=datetime.utcnow())
@@ -44,6 +82,11 @@ class Bot(BotBase):
 				for name ,value,inline in fields:
 					embed.add_field(name=name,value=value,inline=inline)
 				await channel.send(embed=embed)
+				await channel.send(file=File("./data/images/smoke1.jpg"))
+				await self.stdout.send("Stdout hello")
+				while not self.cogs_ready.all_ready():
+					await sleep(0.5)
+				self.ready = True
 			else:
 				print("bot reconnected")
 
